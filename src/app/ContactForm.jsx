de8b57c41e100a3158219e66d421f54a5718f5db
Ejiro Uweghrepha <ejirouweghrepha@gmail.com>
2023-08-09 23:27:13 +0100

    test

--- src/app/ContactForm.jsx
+++ src/app/ContactForm.jsx
@@ -34,26 +34,29 @@ const ContactForm = () => {
   };
 
   return (
+    <div className="bg-white p-8 rounded-lg shadow-md w-96">
+    <h2 className="text-2xl font-semibold mb-4">Contact Us</h2>
     <form onSubmit={handleSubmit}>
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" value={formData.name} onChange={handleChange} />
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700">
+          Name <input type="text" name="name" value={formData.name} onChange={handleChange} className="mt-1 p-2 border rounded-md w-full resize-none focus:ring focus:ring-blue-300" />
         </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" value={formData.email} onChange={handleChange} />
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700">
+          Email <input type="email" name="email" value={formData.email} onChange={handleChange} className="mt-1 p-2 border rounded-md w-full resize-none focus:ring focus:ring-blue-300" />
         </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message" value={formData.message} onChange={handleChange} />
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium text-gray-700">
+          Message <textarea name="message" value={formData.message} onChange={handleChange} rows="4" className="mt-1 p-2 border rounded-md w-full resize-none focus:ring focus:ring-blue-300"/>
         </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+      </div>
+      <div>
+        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Submit</button>
+      </div>
     </form>
+    </div>      
   );
 };
 
