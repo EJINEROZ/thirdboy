b9c0956b979193b0c6e1771a9f2b21c94c45867b
Ejiro Uweghrepha <ejirouweghrepha@gmail.com>
2023-08-09 22:32:18 +0100

    test

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}
-      <form name="contact" netlify="true" hidden>
+      <form name="contact" data-netlify="true" hidden>
       <input type="text" name="name" />
       <input type="email" name="email" />
       <textarea name="message"></textarea>
